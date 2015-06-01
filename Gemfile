1b728fafb8a0ec94c8ade4e9924655b5dd410704
Prashant Vardhan <prashant.vardhan@inveera.us>
2015-06-01 18:34:15 +0530

    add ruby version

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
-
+ruby '2.1.2'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.0'
 # Use sqlite3 as the database for Active Record
@@ -14,6 +14,9 @@ gem 'pg'
 # See https://github.com/sstephenson/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
 gem 'devise'
+gem 'sass-rails', '~> 5.0'
+gem 'coffee-rails', '~> 4.1.0'
+gem 'uglifier', '>= 1.3.0'
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
@@ -60,11 +63,6 @@ group :development, :test do
   
 end
 
-group :assets do
-  gem 'therubyracer'
-  gem 'sass-rails', '~> 5.0'
-  gem 'coffee-rails', '~> 4.1.0'
-  gem 'uglifier', '>= 1.3.0'
-end
+
 
 
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,11 +1,10 @@
-require 'devise'
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
-  # config.secret_key = '5614120980a3f8c2687d068ac89c9e6d6e23c1d0829bf10ccf0dd08e9370983f7682407da2a918c90b593de667b14d6ee2d0a4d8ec8b06edc1f7a885b8120e8e'
+  config.secret_key = '5614120980a3f8c2687d068ac89c9e6d6e23c1d0829bf10ccf0dd08e9370983f7682407da2a918c90b593de667b14d6ee2d0a4d8ec8b06edc1f7a885b8120e8e'
 
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
